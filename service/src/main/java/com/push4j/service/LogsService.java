0010e22b7d395a2f479021887146e55db06b5322
tcrct <tcrct@163.com>
2020-08-04 20:33:38 +0800

    持续开发

--- service/src/main/java/com/push4j/service/LogsService.java
+++ service/src/main/java/com/push4j/service/LogsService.java
@@ -1,10 +1,13 @@
 package com.push4j.service;
 
 import org.fastboot.common.base.BaseController;
+import org.fastboot.common.utils.LogUtils;
 import org.fastboot.db.curd.CurdService;
 import org.fastboot.common.utils.ToolsKit;
 import org.fastboot.exception.common.ServiceException;
 import org.fastboot.exception.nums.ExceptionEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.push4j.dao.LogsDao;
@@ -20,6 +23,8 @@ import com.push4j.entity.LogsEntity;
 */
 @Service
 public class LogsService extends CurdService<LogsEntity> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogsService.class);
 
 	@Autowired
 	private LogsCacheService logsCacheService;
@@ -40,10 +45,14 @@ public class LogsService extends CurdService<LogsEntity> {
 			throw new ServiceException(ExceptionEnum.PARAM_NULL.getCode(), "messageId不能为空");
 		}
 		String sql = "update `log` set `read`=? where `messageId`=?";
-		logsDao.executeUpdate(sql, 0,messageId);
-		logsCacheService.reader(userId, messageId);
-
-
+		try {
+			//更新数据库
+			logsDao.executeUpdate(sql, 0, messageId);
+			// 更新缓存
+			logsCacheService.reader(userId, messageId);
+		} catch (Exception e) {
+			LogUtils.log(LOGGER, e.getMessage(), e);
+		}
 		return ToolsKit.toJsonString(messageId);
 	}
 }
